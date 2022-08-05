a714a505db1df1968a46c860460ea9e49896dd24
unknown <buragohain4@gmail.com>
2022-08-05 18:17:28 +0530

    Implemented Nlog Program

---
+++ NlogProgram/NlogProgram/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NLog;
+
+namespace NlogProgram
+{
+    class Program
+    {
+        public class NLog
+        {
+            private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+            public void LogDebug(string message)
+            {
+                logger.Debug(message);
+            }
+            public void LogError(string message)
+            {
+                logger.Error(message);
+            }
+            public void LogInfo(string message)
+            {
+                logger.Info(message);
+            }
+            public void LogWarn(string message)
+            {
+                logger.Warn(message);
+            }
+
+        }
+
+        public class AddNumbers
+        {
+            private readonly NLog nLog = new NLog();
+
+            public int Sum(int firstNumber, int secondNumber)
+            {
+                { if (firstNumber == 0 || secondNumber == 0)
+                {
+                    nLog.LogDebug("Debug Unsuccessfull : Sum()");
+                    nLog.LogError("Expecting Null values");
+                    nLog.LogWarn("firstNumber or secondNumber should not equal to 0");
+                }
+                int result = firstNumber + secondNumber;
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogInfo("Sum Method Passed, Result :" + result);
+                return result;
+                    }
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter the Firstnum: ");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the Secondnum: ");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+                AddNumbers add = new AddNumbers();
+
+                Console.WriteLine("\nThe sum of two numbers is :" + " " + add.Sum(n1, n2));
+            }  
+        }
+    }
+}
